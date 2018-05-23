e8d9361c9483a44a94b20c90fef4ba6dc4568463
s2h <ssh92kr0410@naver.com>
2018-05-24 00:07:28 +0900

    SearchCityViewController 작성

--- WorldWeather/Controller/SearchCityViewController.swift
+++ WorldWeather/Controller/SearchCityViewController.swift
@@ -13,8 +13,41 @@ protocol SearchCityDelegate {
     func searchNewCity(city: String)
 }
 
-class SearchCityViewController: UIViewController {
+class SearchCityViewController: UIViewController, UITextFieldDelegate {
+    
+    var delegate : SearchCityDelegate?
+    
+    @IBOutlet weak var searchCityTextField: UITextField!
     
     override func viewDidLoad() {
-       
+        searchCityTextField.returnKeyType = .done // 키보드 return -> done
+        searchCityTextField.delegate = self
+    }
+    
+    // 서치버튼 구현
+    @IBAction func searchButton(_ sender: UIButton) {
+        let cityName = searchCityTextField.text!
+        delegate?.searchNewCity(city: cityName)
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    // back 버튼 구현
+    @IBAction func backButton(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    // 화면 터치로 키보드 내리기
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    // return키로 키보드 내리기
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == searchCityTextField {
+            searchCityTextField.resignFirstResponder()
+        } else {
+            
+        }
+        return true
+    }
 }
